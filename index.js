32a330d3f3b339dce860d0f3356897436535d852
Yavuz ÇELİKER <16008116016@ogr.bozok.edu.tr>
2022-11-30 09:51:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -435,22 +435,18 @@ module.exports = {
   },
   // Deprecated Prop Types
   get ColorPropType(): $FlowFixMe {
-    console.warn('');
     return require('deprecated-react-native-prop-types').ColorPropType;
   },
 
   get EdgeInsetsPropType(): $FlowFixMe {
-    console.warn('');
     return require('deprecated-react-native-prop-types').EdgeInsetsPropType;
   },
 
   get PointPropType(): $FlowFixMe {
-    console.warn('');
     return require('deprecated-react-native-prop-types').PointPropType;
   },
 
   get ViewPropTypes(): $FlowFixMe {
-    console.warn('');
     return require('deprecated-react-native-prop-types').ViewPropTypes;
   },
 };
